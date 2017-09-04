3d04b23396d3063b6077f4ec0f8d6de122450217
student <student@M150115>
2017-09-04 12:50:12 +0900

    test

--- multicampus_java_calendar/src/blogtest/CalendarDataManager.java
+++ multicampus_java_calendar/src/blogtest/CalendarDataManager.java
@@ -8,7 +8,7 @@ import java.io.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import javax.swing.*;
-
+////
 class CalendarDataManager { // 6*7배열에 나타낼 달력 값을 구하는 class
 	static final int CAL_WIDTH = 7;
 	final static int CAL_HEIGHT = 6;
