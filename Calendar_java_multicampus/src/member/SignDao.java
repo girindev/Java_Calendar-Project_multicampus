54afc44d791689b4e5af219bbc8379f4c25db8b1
드루와 <31470086+LeeJongRule@users.noreply.github.com>
2017-09-05 15:37:44 +0900

    asd

---
+++ Calendar_java_multicampus/src/member/SignDao.java
@@ -0,0 +1,85 @@
+package member;
+
+import java.sql.*;
+
+public class SignDao {
+	private static final String DB_DRIVER=
+			"com.mqspl.jdbc.Driver";
+	private static final String DB_URL=
+			"jdbc:mysql://127.0.0.1:3306/java";
+	private static final String DB_ID="root";
+	private static final String DB_PW="sds1501";
+	
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public SignDao() {
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally {
+			closeConnection();
+			closePstmt();
+			closeRs();
+		}
+		
+		
+	}
+	public int insertSignDao(SignVo sign)	{
+		int result=0;
+		
+		try {
+			con=DriverManager.getConnection(DB_URL,DB_ID,DB_PW);
+			String sql= "INSERT INTO STUDENTS(ID, PW, NAME, YEAR, MONTH, DAY, PHONE)"+
+					"VALUES(?,?,?,?,?,?,?)";
+			
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, sign.getId());
+			pstmt.setString(2, sign.getPw());
+			pstmt.setString(3, sign.getName());
+			pstmt.setString(4, sign.getYear());
+			pstmt.setString(5, sign.getMonth());
+			pstmt.setString(6, sign.getDay());
+			pstmt.setString(7, sign.getPhon());
+			
+			result=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public void closeConnection() {
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void closePstmt() {
+		if(pstmt!=null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void closeRs() {
+		if(rs!=null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
---
+++ Calendar_java_multicampus/src/member/SignVo.java
@@ -0,0 +1,78 @@
+package member;
+
+public class SignVo {
+	private String numbering;
+	private String id;
+	private String pw;
+	private String name;
+	private String year;
+	private String month;
+	private String day;
+	private String phon;
+	
+	public SignVo() {
+		
+	}
+	public SignVo(String numbering, String id, String pw, String name, 
+			String year, String month, String day, String phon) {
+		this.day=day;
+		this.id=id;
+		this.month=month;
+		this.name=name;
+		this.numbering=numbering;
+		this.phon=phon;
+		this.pw=pw;
+		this.year=year;
+	}
+	public String getNumbering() {
+		return numbering;
+	}
+	public void setNumbering(String numbering) {
+		this.numbering = numbering;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getPhon() {
+		return phon;
+	}
+	public void setPhon(String phon) {
+		this.phon = phon;
+	}
+	
+	
+	
+}
