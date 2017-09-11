5b39c382f1439e9e8f52056b0654e2aff88aba24
student <student@M150115>
2017-09-11 16:20:59 +0900

    2017-09-11 내정보 dao 클래스-김윤경

---
+++ Calendar_java_multicampus/src/member/Info.java
@@ -0,0 +1,8 @@
+package member;
+
+public class Info {
+	public static String id = "ÀÌÁ¾·ü";
+	public static String name = "ÀÌÁ¾·ü";
+	public static boolean selected = true;
+	public static boolean connect = true;
+}
