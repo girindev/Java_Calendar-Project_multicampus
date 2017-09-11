1b3685239327a27d072d269d9edc5f5c998a0110
student <student@M150115>
2017-09-11 16:22:21 +0900

    2017-09-11 회원 리스트 dao 수정- 김윤경

--- Calendar_java_multicampus/src/user/UserVO.java
+++ Calendar_java_multicampus/src/user/UserVO.java
@@ -3,7 +3,6 @@ package user;
 public class UserVO {
 	private String UserID;//아이디
 	private String name;//이름
-	private String str;//
 	private boolean isSelected;//체크여부
 	private boolean isConnect;//접속여부
 	
@@ -11,22 +10,17 @@ public class UserVO {
 		
 	}
 	
-	public UserVO(String str) {
-		this.str = str;
-		isSelected = false;
-		isConnect = false;
-	}
-	
-	
 	public void setUserID(String userID) {
 		UserID = userID;
 	}
 	public String getUserID() {
 		return UserID;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -46,17 +40,8 @@ public class UserVO {
 	public boolean isConnect() {
 		return isConnect;
 	}
-
-	@Override
+	
 	public String toString() {
-		return "UserVO [UserID=" + UserID + ", name=" + name + ", str=" + str + ", isSelected=" + isSelected
-				+ ", isConnect=" + isConnect + "]";
+		return getName();
 	}
-	
-//	public String toString() {
-//		return str;
-//	}
-	
-	
-	
 }
