04c2a39b7918acf603af6a6cb60e483ecac01518
student <student@M150115>
2017-09-11 21:36:31 +0900

    db 채팅 dao - 김윤경

---
+++ Calendar_java_multicampus/src/chattingServer/ChatVO.java
@@ -0,0 +1,23 @@
+package chattingServer;
+
+public class ChatVO {
+	private String msg;
+	private String time;
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
