d8751885916d0ea556b0721edb5465c83b497158
ssunghooni <shcjswo89@naver.com>
2017-09-11 19:28:55 +0900

    스케쥴 관리 Dao - 박성훈

---
+++ Calendar_java_multicampus/src/dao/ScheduleAllSelectDao.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import vo.ScheduleVo;
+
+public class ScheduleAllSelectDao {
+	private ConnectionInit dao;
+	private ResultSet rs = null;
+	
+	public ScheduleAllSelectDao() {
+		dao = new ConnectionInit();
+	}
+	
+	public ArrayList<ScheduleVo> selectScheduleAllList(String date) {
+		new ArrayList<>();
+		ArrayList<ScheduleVo> scheduleList = new ArrayList<>();
+		String sql = "Select * from schedule where sch_date like '"+date+"%'";
+		rs = dao.selectQuery(sql);
+		try {
+			while (rs.next()) {
+				String sch_content = rs.getString("sch_title");
+				Date sch_date = rs.getTimestamp("sch_date");
+				String sch_write_id = rs.getString("sch_write_id");
+				scheduleList.add(new ScheduleVo(sch_content, sch_date, sch_write_id));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return scheduleList;
+	}
+}
