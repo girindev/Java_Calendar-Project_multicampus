d2a0f0fafc07ec44dbc2646bd48c46e8a309b31e
student <student@M150121>
2017-09-12 14:58:34 +0900

    댓글 Dao - 박은정

---
+++ Calendar_java_multicampus/src/dao/ReplyAllSelectDao.java
@@ -0,0 +1,58 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import vo.ReplyVo;
+
+public class ReplyAllSelectDao {
+	private ConnectionInit dao;
+	private ResultSet rs = null;
+	
+	public ReplyAllSelectDao() {
+		dao = new ConnectionInit();
+	}
+	
+	public ArrayList<ReplyVo> selectReplyAllList(String date) {
+		new ArrayList<>();
+		ArrayList<ReplyVo> replyList = new ArrayList<>();
+		String sql = "Select * from comment where com_date like '"+date+"%'";
+		rs = dao.selectQuery(sql);
+		try {
+			while (rs.next()) {
+				String com_comment = rs.getString("com_text");
+				Date com_date = rs.getTimestamp("com_date");
+				String com_write_id = rs.getString("com_write_id");
+				int com_num = rs.getInt("com_num");
+				replyList.add(new ReplyVo(com_comment, com_date, com_write_id,com_num));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return replyList;
+	}
+	
+	public ArrayList<ReplyVo> selectCommentList(int scheduleNum) {
+		new ArrayList<>();
+		ArrayList<ReplyVo> replyList = new ArrayList<>();
+		String sql = "Select * from comment where com_sch_num="+scheduleNum;
+		rs = dao.selectQuery(sql);
+		try {
+			while (rs.next()) {
+				String com_comment = rs.getString("com_text");
+				Date com_date = rs.getTimestamp("com_date");
+				String com_write_id = rs.getString("com_write_id");
+				int com_sch_num = rs.getInt("com_sch_num");
+				replyList.add(new ReplyVo(com_comment, com_date, com_write_id,com_sch_num));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return replyList;
+	}
+	
+}
