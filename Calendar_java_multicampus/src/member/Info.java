164b3e632961bfaf8ad26179d30182393ffc5eb3
드루와 <31470086+LeeJongRule@users.noreply.github.com>
2017-09-12 16:34:42 +0900

    수정사항없음 -이종률

--- Calendar_java_multicampus/src/member/Info.java
+++ Calendar_java_multicampus/src/member/Info.java
@@ -2,8 +2,8 @@ package member;
 
 public class Info {
 
-	public static String id = "ÀÌÁ¾·ü";
-	public static String name = "ÀÌÁ¾·ü";
+	public static String id="";
+	public static String name="";
 	public static boolean selected = true;
 	public static boolean connect = true;
 	
