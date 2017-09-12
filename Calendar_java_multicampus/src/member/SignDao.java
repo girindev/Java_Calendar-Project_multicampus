3275fc8dd9700a45ba354ac4f5611393d170b5c2
드루와 <31470086+LeeJongRule@users.noreply.github.com>
2017-09-12 15:27:24 +0900

    이종률

--- Calendar_java_multicampus/src/member/SignDao.java
+++ Calendar_java_multicampus/src/member/SignDao.java
@@ -6,7 +6,7 @@ public class SignDao {
 	private static final String DB_DRIVER=
 			"com.mysql.jdbc.Driver";
 	private static final String DB_URL=
-			"jdbc:mysql://127.0.0.1:3306/java";
+			"jdbc:mysql://127.0.0.1:3306/project";
 	private static final String DB_ID="root";
 	private static final String DB_PW="sds1501";
 	
