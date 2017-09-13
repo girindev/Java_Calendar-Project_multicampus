e24cabd5a47eaaaa432cdd7fd610b348740814fb
student <student@M150121>
2017-09-13 09:31:27 +0900

    스케줄 수정 DAO - 박은정

---
+++ Calendar_java_multicampus/src/popUp/ModifyScheduleDAO.java
@@ -0,0 +1,56 @@
+package popUp;
+
+import java.sql.*;
+// calendar(이름 임의지정) 테이블에 SQL을 전달하는 DB작업들을 담당하는 클래스
+
+public class ModifyScheduleDAO {
+	private static final String DRIVER_NAME = 
+			"com.mysql.jdbc.Driver";
+	private static final String DB_URL = 
+			"jdbc:mysql://127.0.0.1:3306/project";
+	private static final String DB_ID = "root";
+	private static final String DB_PW = "sds1501";
+	
+	public ModifyScheduleDAO(){
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 오류/jar파일 또는 스트링 확인 요망");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int update(String sch, String date, int sch_num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		try {
+			con = DriverManager.getConnection(DB_URL, DB_ID, DB_PW);
+			String sql = "UPDATE SCHEDULE SET SCH_DATE=?, SCH_TITLE=? WHERE SCH_NUM=?";
+					
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, date);
+			pstmt.setString(2, sch);
+			pstmt.setInt(3, sch_num);
+			
+			result = pstmt.executeUpdate();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(pstmt!=null)
+				try {pstmt.close();} catch (SQLException e) {}
+			if(con!=null)
+				try {con.close();} catch (SQLException e) {}
+		}
+		return result;
+		
+	}
+	
+}
+
+
+
